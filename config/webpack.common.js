7b23219a233fb063b24dfc98f3c3bd3c364c030e
horse <littlehorseboy@gmail.com>
2019-09-17 17:38:30 +0800

    feat:【Day 02】從一個空白資料夾開始 tsc init webpack.common.js webpack-merge .gitignore

---
+++ config/webpack.common.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.join(__dirname, '../src/index.ts'),
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, '../dist'),
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        use: [
+          {
+            loader: 'ts-loader',
+          },
+        ],
+      },
+    ],
+  },
+};
