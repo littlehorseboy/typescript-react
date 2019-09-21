83d82ca0d715caed3f64834f8fe078f18fbd5960
littlehorseboy <littlehorseboy@gmail.com>
2019-09-21 10:54:35 +0800

    refactor: component 換個目錄放

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import Counter from './components/Counter/Counter';
-import GetUUID from './components/GetUUID/GetUUID';
-import GetUUIDByMultipleOfFive from './components/GetUUIDByMultipleOfFive/GetUUIDByMultipleOfFive';
+import Counter from './components/day05/Counter/Counter';
+import GetUUID from './components/day05/GetUUID/GetUUID';
+import GetUUIDByMultipleOfFive from './components/day05/GetUUIDByMultipleOfFive/GetUUIDByMultipleOfFive';
 
 export default function Root(): JSX.Element {
   return (
