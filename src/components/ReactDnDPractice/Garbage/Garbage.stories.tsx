3032a898a3f9047c4b781a60fe06f94cf76989f4
littlehorseboy <littlehorseboy@gmail.com>
2019-10-06 22:19:25 +0800

    feat: 【Day 21】Storybook，Addons - Knobs

--- src/components/ReactDnDPractice/Garbage/Garbage.stories.tsx
+++ src/components/ReactDnDPractice/Garbage/Garbage.stories.tsx
@@ -4,11 +4,13 @@ import HTML5Backend from 'react-dnd-html5-backend';
 /* eslint-disable import/no-extraneous-dependencies */
 import { storiesOf } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
+import { withKnobs, text } from '@storybook/addon-knobs';
 /* eslint-enable */
 import Garbage from './Garbage';
 
 storiesOf('Garbage', module)
+  .addDecorator(withKnobs)
   .addDecorator((story) => <DndProvider backend={HTML5Backend}>{story()}</DndProvider>)
   .add('Garbage', withInfo()(() => (
-    <Garbage name="garbage" />
+    <Garbage name={text('Name', 'garbage')} />
   )));
